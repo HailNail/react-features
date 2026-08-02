b96b730425c8c61d1146c1c2256008f07908d001
HailNail <theproducer228@gmail.com>
2026-08-02 17:47:57 +0500

    fixed big list in react18 filterList

--- src/hooks/useFilterListWithPagination.ts
+++ src/hooks/useFilterListWithPagination.ts
@@ -1,14 +1,18 @@
-import React, { useDeferredValue, useMemo } from 'react';
+import React, { useDeferredValue, useMemo, useState } from 'react';
 import useLocalStorage from './useLocalStorage';
 import { USER_DATA } from '../utils/generateLargeUserList';
 
+const ITEMS_FOR_PAGE = 50;
+
 const useFilterListWithPagination = () => {
   const [inputValue, setInputValue] = useLocalStorage('search_term', '', 300);
+  const [page, setPage] = useState(1);
 
   const deferred = useDeferredValue(inputValue);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
+    setPage(1);
   };
 
   const filteredUsers = useMemo(() => {
@@ -20,7 +24,14 @@ const useFilterListWithPagination = () => {
     );
   }, [deferred]);
 
-  return { inputValue, handleSearchChange, filteredUsers };
+  const paginatedUsers = useMemo(() => {
+    const staerIndex = (page - 1) * ITEMS_FOR_PAGE;
+    return filteredUsers.slice(staerIndex, staerIndex + ITEMS_FOR_PAGE)
+  }, [filteredUsers, page]);
+
+  const totalPages = Math.ceil(filteredUsers.length / ITEMS_FOR_PAGE);
+
+  return { inputValue, page, setPage, handleSearchChange, paginatedUsers, totalPages };
 };
 
 export default useFilterListWithPagination;
